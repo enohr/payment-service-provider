78f066b8cd6264594556a037bfd9c7620fec6d56
Eduardo Lessa <eduardonohr@gmail.com>
2021-03-15 23:23:18 -0300

    Feat: Swagger added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const swagger = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
+
 const router = require('./routes')
-
 const database = require ('./database');
-
 const { errorHandler } = require('./middlewares/errorMiddleware');
+const swaggerDocument = YAML.load(path.resolve(__dirname, '..', 'swagger.yaml'));
+
 const app = express()
 
 app.use(express.json());
@@ -11,5 +15,7 @@ app.use(router);
 app.use((err, req, res, next) => {
     errorHandler(err, res);
 })
+
+app.use('/api-docs', swagger.serve, swagger.setup(swaggerDocument, {explorer: true}));
 
 module.exports = app;
